f587685a937a1facf66f9ed49530136f98575ed2
Erick Wendel <erick.workspace@gmail.com>
2023-01-04 10:28:57 -0300

    remove gm dependency to work directly with the binary
    Signed-off-by: Erick Wendel <erick.workspace@gmail.com>

--- examples/example.js
+++ examples/example.js
@@ -3,8 +3,8 @@ const memeMaker = require('../meme-maker')
 const options = {
   image: './spiderman.jpeg',         // Required
   outfile: 'spiderman-meme.png',  // Required
-  topText: 'TODAY IM',            // Required
-  bottomText: 'AN ASS',           // Optional
+  topText: 'AE',            // Required
+  bottomText: 'aa ASS',           // Optional
   font: './../impact.ttf',      // Optional
   fontSize: 50,                   // Optional
   fontFill: '#FFF',               // Optional
